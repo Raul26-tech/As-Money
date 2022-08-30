c31ad972ca580a291f22f5704bbe2461ffa974d3
Raul Santos <rauletec2020@gmail.com>
2022-08-30 20:43:31 -0300

    Ajustando página home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,24 @@
+import 'animate.css';
+import IconIlustrator from '../../assets/iconMoney.svg';
+
 export default function Home() {
-    return <h2>Home</h2>;
+    return (
+        <>
+            <section className="flex-1 h-full translate-y-full p-5 justify-center items-center ">
+                <div className="flex justify-center items-center text">
+                    <span className="animate__animated animate__rubberBand flex justify-center items-center text-center md:text-xl">
+                        Seja bem vindo(a) ao Ass-Money, a melhor maneira de
+                        contabilizar os gastos
+                    </span>
+                </div>
+            </section>
+            <div className="w-full flex justify-center items-center mt-14 md:mt-12">
+                <img
+                    className="animate__animated animate__rubberBand flex justify-center items-center"
+                    src={IconIlustrator}
+                    alt="Ilustração"
+                />
+            </div>
+        </>
+    );
 }
