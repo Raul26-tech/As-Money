3c663e84231b94a75b5dd51ad27ef1547f58fbe8
Raul26-tech <rauletec2020@gmail.com>
2022-09-03 16:14:47 -0300

    Ajustando responsividade do botão de transactions

--- src/pages/MonthlyValues/index.tsx
+++ src/pages/MonthlyValues/index.tsx
@@ -3,13 +3,15 @@ import { AiOutlinePlus } from 'react-icons/ai';
 export default function MonthlyVAlues() {
     return (
         <div className="w-full h-14 p-3 flex justify-end text-center">
-            <div className="flex h-[4rem] p-3">
+            <div className="flex w-[10rem] md:w-full justify-end h-[4rem] p-2">
                 <button className="w-[14rem] h-full flex justify-center text-center items-center text-white bg-btn-transaction hover:bg-btn-transaction-hover rounded-md p-2 shadow-lg ">
                     <AiOutlinePlus
                         size={20}
                         style={{ marginRight: '0.25rem' }}
                     />
-                    Adicionar nova transação
+                    <span className="text-[0.70rem] md:text-base">
+                        Adicionar nova transação
+                    </span>
                 </button>
             </div>
         </div>
