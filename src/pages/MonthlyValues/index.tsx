3db5b25d43450b340c1843fa770ce51d97917abc
Raul Santos <rauletec2020@gmail.com>
2022-09-26 17:32:51 -0300

    Finalizando estilização do form

--- src/pages/MonthlyValues/index.tsx
+++ src/pages/MonthlyValues/index.tsx
@@ -81,6 +81,20 @@ export default function MonthlyVAlues() {
                                 {...register('type')}
                             />
                         </CheckboxContainer>
+                        <div className="mt-6 flex justify-center items-center">
+                            <button
+                                type="submit"
+                                className="w-full h-full flex  justify-center text-center items-center text-white bg-btn-transaction hover:bg-btn-transaction-hover rounded-md p-2 shadow-lg "
+                            >
+                                <AiOutlinePlus
+                                    size={20}
+                                    style={{ marginRight: '0.25rem' }}
+                                />
+                                <span className="text-[0.70rem] md:text-base">
+                                    Enviar
+                                </span>
+                            </button>
+                        </div>
                     </form>
                 </Modal>
             )}
