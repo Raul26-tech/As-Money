e9ebb795601fdca3e2b65af5dc99bae0b9be303e
Raul Santos <rauletec2020@gmail.com>
2022-10-26 18:38:42 -0300

    Ajustando Layout da page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,21 +1,24 @@
 import 'animate.css';
 import IconIlustrator from '../../assets/iconMoney.svg';
+import { Container } from '../../components/Container';
 import Content from '../../components/Content';
 
 export default function Home() {
     return (
         <Content>
-            <section className="flex flex-col w-full h-full justify-center items-center">
-                <img
-                    className="animate__animated animate__rubberBand"
-                    src={IconIlustrator}
-                    alt="Ilustração"
-                />
-                <p className="animate__animated animate__rubberBand text-center text-sm md:text-xl ">
-                    Seja bem vindo(a) ao Ass-Money, a melhor maneira de
-                    contabilizar os gastos
-                </p>
-            </section>
+            <Container>
+                <section className="flex flex-col w-full h-full justify-center items-center">
+                    <img
+                        className="animate__animated animate__rubberBand"
+                        src={IconIlustrator}
+                        alt="Ilustração"
+                    />
+                    <p className="animate__animated animate__rubberBand text-center text-sm md:text-xl ">
+                        Seja bem vindo(a) ao Ass-Money, a melhor maneira de
+                        contabilizar os gastos
+                    </p>
+                </section>
+            </Container>
         </Content>
     );
 }
